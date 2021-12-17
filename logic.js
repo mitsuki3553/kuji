44bef6eb814f199a56bcd2151a53b36e4849cdc9
mitsuki <mitsuki3553@gmail.com>
2021-12-17 14:58:29 +0900

    ✨ ボタン押下時の実装

--- logic.js
+++ logic.js
@@ -1,3 +1,4 @@
+//名簿
 const memberList = [
   "今井さん",
   "河田さん",
@@ -17,13 +18,57 @@ const memberList = [
   "櫻井さん",
 ];
 
-const member = document.getElementById("member");
+//もらってない人
+const wait = [...memberList];
+//残りのプレゼント
+const gift = [...memberList];
 
+//もらった後の組み合わせ
+const matching = [];
+
+const member = document.getElementById("member");
+const from = document.getElementById("from");
+const to = document.getElementById("to");
+const btn = document.getElementById("btn");
+
+//ロード時の実装
 window.onload = () => {
-  memberList.map((i) => {
+  memberList.map((item, index) => {
+    //タグを作成して属性を付与
     const div = document.createElement("div");
-    div.innerText = i;
-    div.id = i;
+    div.innerText = item;
+    div.id = item;
+    div.className = "pointer";
+    div.onclick = () => addFrom(item, index);
+    //要素追加
     member.appendChild(div);
   });
 };
+
+//「誰の」のところに名前追加
+function addFrom(item, index) {
+  from.innerHTML = item;
+  wait.splice(index, 1);
+  btn.disabled = false;
+}
+
+//ボタンを押したとき
+// ①乱数生成
+// ②乱数に対応する人をピックアップ
+// ③「誰に」に表示
+// ④配列から削除
+// ⑤組み合わせに追加
+// ⑥「誰の」「誰に」を削除
+
+btn.addEventListener("click", () => addTo());
+function addTo() {
+  //もらってない人がいなくなったらボタンを押せなくする
+  if (gift.length === 0) {
+    btn.disabled = true;
+  }
+  const random = Math.floor(Math.random() * gift.length); //①
+  const takeGift = gift[random]; //②
+  to.innerHTML = takeGift; //③
+  gift.splice(random, 1); //④
+  console.log(gift.length);
+}
