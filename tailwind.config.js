3706b7c278eb7f05075ec11ed282f9e633e6815b
mitsuki <mitsuki3553@gmail.com>
2021-12-19 20:32:35 +0900

    🎨 背景画像追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
     extend: {
       backgroundImage: (theme) => ({
         christmas1: "url('./images/christmas1.jpg')",
+        present1: "url('./images/present1.png')",
+        present: "url('./images/present.png')",
       }),
     },
   },
